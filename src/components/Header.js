c6e64748f4f13ee0bb143bf0b07b9fb3ed68f484
tHALL3000 <tiffanyhall.co@gmail.com>
2021-10-06 06:10:51 -0600

    Skeleton for header section complete.
    Class names and BEM file structure created. next step: styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react";
 
 function Header() {
-    return(
-    //html goes here
-    )
+	return (
+		<header className="header">
+			<image className="header__logo">
+				{" "}
+			</image>
+			<nav className="header__nav">
+				<ul className="header__nav-links">
+					<li className="section__link_plain"></li>
+					<li className="section__link_pink"></li>
+					<li className="section__link_plain"></li>
+				</ul>
+				<menu className="header__menu">
+					<div></div>
+					<div></div>
+					<div></div>
+				</menu>
+				<a
+					className="section__link_icon"
+					href="#"
+				>
+					icon
+				</a>
+			</nav>
+		</header>
+	);
 }
 
 export default Header;
