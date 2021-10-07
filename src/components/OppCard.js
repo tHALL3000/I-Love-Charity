53ec1bc18b44edc618b7b4b81be8bb0f7bce8aa9
Tristan Boyd <trisboyd@gmail.com>
2021-10-06 22:16:53 -0600

    'oppportunities setup'

--- src/components/OppCard.js
+++ src/components/OppCard.js
@@ -1,10 +1,25 @@
 import React from "react";
+import heart from '../images/donate-heart.png';
 
-function OppCard() {
-	// return(
-	// //html goes here
-	// // this is an individual card in the opportunities near you section
-	// )
+function OppCard(props) {
+
+	return (
+		<article className='opp-card'>
+			<div className='opp-card__image' style={{ background: `${props.picture}` }}>
+				<div className='opp-card__image-info'>
+					<p className='opp-card__image-text'>Raised <br /> {props.raiseAmount}</p>
+					<p className='opp-card__image-text'>Need to collect <br />{props.needAmount}</p>
+				</div>
+			</div>
+			<p className='opp-card__text opp-card__text_faint'>{props.topic}</p>
+			<p className='opp-card__text opp-card__text_bold'>{props.title}</p>
+			<p className='opp-card__text opp-card__text_normal'>{props.about}</p>
+			<button className='opp-card__button'>
+				<p className='opp-card__text opp-card__text_normal'>Donate</p>
+				<image className='opp-card__heart' src={heart} />
+			</button>
+		</article>
+	)
 }
 
 export default OppCard;
---
+++ src/utilities/opps.js
@@ -0,0 +1,24 @@
+import puppy from '../images/puppy.jpg';
+import parent from '../images/parent.jpg';
+import massage from '../images/massage.jpg';
+
+export const opps = [
+    {
+        picture: {puppy},
+        topic: 'animals',
+        title: 'New Puppy Center for Cute Pups',
+        about: 'Started construction of center that will house 150 cute puppies for people'
+    },
+    {
+        picture: {massage},
+        topic: 'programmers',
+        title: 'Massage Center for stressed coders',
+        about: 'Found location and began hiring process for world class masseuses'
+    },
+    {
+        picture: {parent},
+        topic: 'children',
+        title: 'Homecare service for working parents',
+        about: 'Began hiring process for workers to watch kids for parents who need to complete this website and others'
+    }
+]
