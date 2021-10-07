59d5cbe5ded5a7a4906c878e579910df2e04858e
Tristan Boyd <trisboyd@gmail.com>
2021-10-07 11:08:48 -0600

    'opportunities finished'

--- src/components/OppCard.js
+++ src/components/OppCard.js
@@ -5,18 +5,19 @@ function OppCard(props) {
 
 	return (
 		<article className='opp-card'>
-			<div className='opp-card__image' style={{ background: `${props.picture}` }}>
+			<div className='opp-card__image-container'>
+				<img className='opp-card__image' src={props.picture} alt='pic' />
 				<div className='opp-card__image-info'>
 					<p className='opp-card__image-text'>Raised <br /> {props.raiseAmount}</p>
 					<p className='opp-card__image-text'>Need to collect <br />{props.needAmount}</p>
 				</div>
 			</div>
-			<p className='opp-card__text opp-card__text_faint'>{props.topic}</p>
-			<p className='opp-card__text opp-card__text_bold'>{props.title}</p>
-			<p className='opp-card__text opp-card__text_normal'>{props.about}</p>
+				<p className='opp-card__text opp-card__text_faint'>{props.topic}</p>
+				<p className='opp-card__text opp-card__text_bold'>{props.title}</p>
+				<p className='opp-card__text opp-card__text_normal opp-card__text_paragraph'>{props.about}</p>
 			<button className='opp-card__button'>
-				<p className='opp-card__text opp-card__text_normal'>Donate</p>
-				<image className='opp-card__heart' src={heart} />
+				<p className='opp-card__text opp-card__text_normal opp-card__text_button'>Donate</p>
+				<img className='opp-card__heart' src={heart} alt='heart' />
 			</button>
 		</article>
 	)
--- src/utilities/opps.js
+++ src/utilities/opps.js
@@ -1,24 +1,20 @@
-import puppy from '../images/puppy.jpg';
-import parent from '../images/parent.jpg';
-import massage from '../images/massage.jpg';
-
 export const opps = [
     {
-        picture: {puppy},
+        picture: 'https://images.unsplash.com/photo-1593134257782-e89567b7718a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
         topic: 'animals',
         title: 'New Puppy Center for Cute Pups',
         about: 'Started construction of center that will house 150 cute puppies for people'
     },
     {
-        picture: {massage},
+        picture: 'https://images.unsplash.com/photo-1519823551278-64ac92734fb1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
         topic: 'programmers',
         title: 'Massage Center for stressed coders',
         about: 'Found location and began hiring process for world class masseuses'
     },
     {
-        picture: {parent},
+        picture: 'https://images.unsplash.com/photo-1503454537195-1dcabb73ffb9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=386&q=80',
         topic: 'children',
         title: 'Homecare service for working parents',
-        about: 'Began hiring process for workers to watch kids for parents who need to complete this website and others'
+        about: 'Began hiring workers to watch kids for parents who need to finish this website'
     }
 ]
