e24b5f5966d17937f009faf49e7cf2da0fe7f9d2
tHALL3000 <tiffanyhall.co@gmail.com>
2021-10-06 20:55:23 -0600

    keyframes added to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Hero() {
-	<div className="hero"></div>;
+	<div className="hero">
+		<h1>
+			ILOVECHARITY
+		</h1>
+		<h3>
+			Helping the needy under <span>any circumstances</span>
+			<img src="./images/Vectorfilled-heart-white"
+		</h3>
+	</div>;
 }
 
 export default Hero;
